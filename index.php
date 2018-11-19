b959e537fa5e8e5dadaff73e31dc495e0fdd6d62
khoursaaf <khoursa@gmail.com>
2018-11-19 10:16:41 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title>Exercise</title>
+</head>
+<body>
+  <p>En allant de 20 à 0 avec un pas de 1, afficher le message C'est presque bon.</p>
+  <?php
+  $a = 20;
+  while ($a >= 0) {
+    echo $a."<br>";
+    $a--;
+  }
+   ?>
+</body>
+</html>
