3e6c5ab0b1efda8750d0f76f2c27cab5e679c0d7
khoursaaf <khoursa@gmail.com>
2018-11-20 16:26:45 +0100

    correction

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <meta charset="utf-8" />
-  <title>Exercise</title>
-</head>
-<body>
-  <p>En allant de 20 à 0 avec un pas de 1, afficher le message C'est presque bon.</p>
-  <?php
-  $a = 20;
-  while ($a >= 0) {
-    echo $a."<br>";
-    $a--;
-  }
-   ?>
-</body>
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercise</title>
+  </head>
+  <body>
+    <p>En allant de 20 à 0 avec un pas de 1, afficher le message "C'est presque bon".</p>
+    <?php
+    $number = 20;
+    while ($number >= 0) { ?>
+    <p><?php echo $number.' '.'C\'est presque bon'; ?></p>
+    <?php $number--; } ?>
+  </body>
 </html>
